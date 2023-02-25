e2226f45ef8c32de6330ca7c5fee99bdddd54873
Amdrii <andreyk557@gmail.com>
2023-02-25 19:53:00 +0200

    feat[func-paint]

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 const canvas = document.getElementById('jsCanvas')
+const ctx = canvas.getContext('2d')
+
+
+canvas.height = 700;
+canvas.width = 700;
+
+ctx.lineWidth = 2.5
+ctx.strokeStyle = 'black';
 
 let painting = false;
 
+function stopPainting() {
+    painting = false
+}
+
+function startPainting(event) {
+
+}
+
 
 function onMouseMove(event) {
-    x = event.offsetX
-    y = event.offsetY
-    console.log(x, y)
+    x = event.offsetX;
+    y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+    } else {
+        ctx.lineTo(x, y)
+        ctx.stroke()
+    }
 }
 
 function onMouseDown(event){
-
-}
-
-function onMouseUp(event) {
-    
+painting = true
 }
 
 if(canvas) {
     canvas.addEventListener('mousemove',  onMouseMove)
     canvas.addEventListener('mousedown', onMouseDown)
-    canvas.addEventListener('mouseup', onMouseUp)
+    canvas.addEventListener('mouseup', startPainting)
+    canvas.addEventListener('mouseleave', stopPainting)
 }
+
+
+
 
 
 
